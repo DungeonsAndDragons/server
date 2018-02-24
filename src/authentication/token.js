87c4ec1851bab2d2c6b3b28eb921021bb3b5102c
Til Blechschmidt <til@blechschmidt.de>
2018-02-25 00:32:53 +0100

    Implemented password check

--- src/authentication/token.js
+++ src/authentication/token.js
@@ -2,19 +2,29 @@ import { sign, verify } from 'jsonwebtoken'
 import bearerToken from 'express-bearer-token'
 
 import config from '../../config.json'
+import { database } from '../db'
 
 const secret = config.token.secret;
 
 export function generateDevelopmentToken() {
-    return sign({ id: 'superSpecificUserID' }, secret);
+    return sign({ id: 1 }, secret);
 }
 
 export function registerTokenResource(app) {
     app.use(bearerToken());
-    app.get('/token', (req, res) => {
-        // TODO Verify user details and do other stuff
-        var token = sign({ id: 'superSpecificUserID' }, secret, { expiresIn: config.token.validityPeriod });
-        res.send(token);
+    app.get('/token', async (req, res) => {
+        try {
+            const db = await database;
+            const { id, password } = await db.get('SELECT id, password FROM Players WHERE name = ?', req.query.username);
+
+            if (password !== req.query.password) throw new Error("Invalid password");
+
+            const token = sign({ id }, secret, { expiresIn: config.token.validityPeriod });
+            res.send(token);
+        } catch (err) {
+            res.status(403);
+            res.send("Unknown player or invalid password.");
+        }
     })
 }
 
